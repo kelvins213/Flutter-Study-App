8018dfbac596644a8631154d5cf1b309bfbbbff5
thayingrid <tip1@aluno.ifal.edu.br>
2022-09-09 07:49:08 -0300

    project commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:untitled4/pages/card_materias.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: CardMaterias(),
+    ),
+  );
+}
